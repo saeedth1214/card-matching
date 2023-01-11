2936c6415a7f0871f2b957f89fd07d3217c68ad0
saeedsoltani <m.h.soltani1214@gmail.com>
2023-01-11 21:57:05 +0330

    init project and scaffold game board.

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup></script>
+
+<template>
+  <h1>Peek-a-Vue</h1>
+
+  <section class="game-board">
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+  </section>
+</template>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 60px;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.card {
+  border: 3px solid #ccc;
+}
+.game-board {
+  display: grid;
+  grid-template-columns: 100px 100px 100px 100px;
+  grid-template-rows: 100px 100px 100px 100px;
+  grid-column-gap: 30px;
+  grid-row-gap: 30px;
+  justify-content: center;
+}
+</style>
